cae4947e8f70b7b14221e84af7acb240318068c8
BerLevi <levente.berenyi@mindtechapps.com>
2021-11-29 18:09:27 +0100

    feat: added environmental variables

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from "dotenv";
 import postRoutes from "./routes/clothes.js";
 
 const app = express();
+dotenv.config();
 
 //set up bodyParser so it can properly send the requests
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -17,14 +18,10 @@ app.use(cors());
 //every route inside the post route will start with post
 app.use("/clothes", postRoutes);
 
-//ToDo store connection string in environmental variable
-//const CONNECTION_URL = ("mongodb+srv://berenyi_levente:westend6@cluster0.1ilkc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-const CONNECTION_URL = "mongodb://localhost:27017/eturi";
-
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
